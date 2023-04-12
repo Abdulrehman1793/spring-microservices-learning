5f3fa4fc412e6f7c3ec7bcd05c5aedd8e3266c8d
KHAN ABDULREHMAN <abdulrehman1793@gmail.com>
2023-04-12 11:36:55 +0530

    Implemented fraud check from customer module

--- customer/src/main/java/com/abdulrehman1793/customer/CustomerService.java
+++ customer/src/main/java/com/abdulrehman1793/customer/CustomerService.java
@@ -2,11 +2,13 @@ package com.abdulrehman1793.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -15,5 +17,17 @@ public class CustomerService {
                 .email(request.email())
                 .build();
         customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        assert fraudCheckResponse != null;
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
---
+++ customer/src/main/java/com/abdulrehman1793/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.abdulrehman1793.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
