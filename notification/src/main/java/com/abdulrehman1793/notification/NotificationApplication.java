a53209b685bc57fdd5b9929034365a24c29ef83a
KHAN ABDULREHMAN <abdulrehman1793@gmail.com>
2023-04-14 12:17:54 +0530

    Implemented rabbit mq

--- notification/src/main/java/com/abdulrehman1793/notification/NotificationApplication.java
+++ notification/src/main/java/com/abdulrehman1793/notification/NotificationApplication.java
@@ -1,13 +1,38 @@
 package com.abdulrehman1793.notification;
 
+//import com.abdulrehman1793.amqp.RabbitMQMessageProducer;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {
+        "com.abdulrehman1793.notification",
+        "com.abdulrehman1793.amqp",
+})
+//@EnableDiscoveryClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//        @Bean
+//        CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//            ) {
+//        return args -> {
+//            producer.publish(
+//                    "foo",
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//            producer.publish(
+//                    new Person("Abdulrehman", 30),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
